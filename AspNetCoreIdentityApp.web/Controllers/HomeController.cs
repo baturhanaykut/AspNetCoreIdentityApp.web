94c6f4c2773c529581c1d679779487929c68f971
Baturhan Aykut <baturhanaykut@gmail.com>
2023-07-03 23:17:54 +0300

    Ders-11 Sonu

--- AspNetCoreIdentityApp.web/Controllers/HomeController.cs
+++ AspNetCoreIdentityApp.web/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 ﻿using AspNetCoreIdentityApp.web.Models;
+using AspNetCoreIdentityApp.web.ViewModels;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -7,10 +9,12 @@ namespace AspNetCoreIdentityApp.web.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly UserManager<AppUser> _userManager;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, UserManager<AppUser> userManager)
         {
             _logger = logger;
+            _userManager = userManager;
         }
 
         public IActionResult Index()
@@ -22,6 +26,31 @@ namespace AspNetCoreIdentityApp.web.Controllers
         {
             return View();
         }
+
+        public IActionResult SignUp()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignUp(SignUpViewModel request)
+        {
+            var identityResult = await _userManager.CreateAsync(new() { UserName = request.UserName, PhoneNumber = request.Phone, Email = request.Email }, request.PasswordConfirm);
+
+            if (identityResult.Succeeded)
+            {
+                TempData["SuccessMessage"] = "Üyelik kayıt işlemi başarıylar gerçekleşmiştir.";
+                return RedirectToAction(nameof(HomeController.SignUp));
+            }
+            foreach (IdentityError item in identityResult.Errors)
+            {
+                ModelState.AddModelError(string.Empty, item.Description);
+            }
+            
+            return View();
+        }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
---
+++ AspNetCoreIdentityApp.web/Models/AppUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace AspNetCoreIdentityApp.web.Models
+{
+    public class AppUser:IdentityUser
+    {
+    }
+}
